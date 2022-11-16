487e854680782d71c7da52c01e5730de261db42a
Ksawery <pawlikksawery@gmail.com>
2022-11-16 18:29:08 +0100

    egzamin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ChipComponent } from './components/chip/chip.component';
+import { ChipFormComponent } from './components/chip-form/chip-form.component';
+import { ChipComponentModule } from './components/chip/chip.component-module';
+import { ChipServiceModule } from './services/chip.service-module';
+import { ChipFormComponentModule } from './components/chip-form/chip-form.component-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([{ path: 'chip', component: ChipComponent }, { path: 'chipForm', component: ChipFormComponent }]), ChipComponentModule, ChipServiceModule, ChipFormComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from "@angular/common/http";
+import {ChipComponentModule} from "./components/chip/chip.component-module";
+import {ChipFormComponentModule} from "./components/chip-form/chip-form.component-module";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HttpClientModule } from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    ChipComponentModule,
+    ChipFormComponentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
